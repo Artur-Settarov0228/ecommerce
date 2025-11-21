f688a126eec598efef1a2c5573a1a1cf604ad1d9
djumanov <djumanovdev@gmail.com>
2025-11-21 16:48:02 +0500

    refuctor

--- core/settings.py
+++ core/settings.py
@@ -41,9 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # Local Apps
-    'accounts.apps.AccountsConfig',
-    'products.apps.ProductsConfig',
-    'orders.apps.OrdersConfig',
+    'apps.accounts.apps.AccountsConfig',
+    'apps.products.apps.ProductsConfig',
+    'apps.orders.apps.OrdersConfig',
 ]
 
 MIDDLEWARE = [
